31d5976e4d5dd02076e85f776d1f93120a42b718
Alfrey-Chan <122188063+Alfrey-Chan@users.noreply.github.com>
2026-01-01 17:24:36 +0900

    chore(config): add AWS S3 configuration

--- config/services.php
+++ config/services.php
@@ -22,6 +22,13 @@ return [
         'key' => env('RESEND_API_KEY'),
     ],
 
+    'aws' => [
+        'key' => env('AWS_ACCESS_KEY_ID'),
+        'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+        'bucket' => env('AWS_BUCKET'),
+    ],
+
     'ses' => [
         'key' => env('AWS_ACCESS_KEY_ID'),
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
